08919232ea1b9fa99c1e3c1c82110641dc1a93c2
Van Tam Nguyen <tam.nguyen@ufz.de>
2020-12-29 09:17:38 +0100

    Initial

---
+++ server.R
@@ -0,0 +1,205 @@
+
+# ------------------------------------------------------------------------------
+# Creating server
+server <- function(input, output,session) {
+  
+  # Project information
+  output$projectInfo <- renderUI({ 
+    str1 <- paste("Project information")
+    str2 <- paste("Similation time:", "01.01.1990", "-", "31.12.1991")
+    str3 <- paste("NSKIP", "2 years")
+    HTML(paste(str1, str2, str3, sep = '<br/>'))
+    
+  })
+  
+  #Plot HRU map
+  output$plotHRUmap <- renderPlot({
+    hru_raster <-  raster(paste(input$Watershed, "/Grid/hrus1/w001001.adf", sep = ""))
+    plot(hru_raster,legend.args = list(text = 'HRU number', side = 4, 
+                                       font = 1, line = 2.5, cex = 0.8))
+  })
+  
+  #Plot Subbasin
+#  output$plotSubbasin <- renderPlot({
+#    subbasin <-  shapefile(paste(input$Watershed, "/Shapes/subs1.shp", sep = ""))
+#    plot(subbasin)
+#  })
+  
+  #Plot Simulated Streamflow
+  output$plotQ <- renderPlot({
+    watout <- read.table("./testData/TxtInOut/watout.dat" , header = FALSE,
+                         sep = "",skip = 6)
+    start.date <- as.Date(paste(watout[1,1],"0101", sep=""), "%Y%m%d")
+    start.date <- start.date + watout[1,2] - 1
+    
+    time <- seq(start.date, start.date + length(watout[,1])-1, by ="days")
+    watout.extract <- watout[,4]
+    
+    s.date <- which(time == input$dateRange[1])
+    e.date <- which(time == input$dateRange[2])
+    
+    plot(time[s.date:e.date], watout.extract[s.date:e.date], type ="l", col = 2)
+  })
+  
+  # ----------------------------------------------------------------------------
+ 
+  # --------------------------------------------------------Read hru raster file
+  hruRaster <- reactive({
+    raster(paste(input$visualWatershed, 
+                 "/Grid/hrus1/w001001.adf", 
+                 sep = ""
+                 )
+           )
+  })  
+
+  # --------------------------------------------------------------Get HRU header
+  HRUheader <- reactive({
+    getHRUheader(input$visualTxtInOut)
+  })
+  
+  # -------------------------------------------------------Get simulation period 
+  sim_period <- reactive({
+    getDate(input$visualTxtInOut)
+  })
+
+  #*****************************************************************************
+  # Update date range hru plot    
+  observe({
+    mydate <- sim_period()
+    updateDateRangeInput(session, "date_range_hru",
+                         start = mydate[2],
+                         end = mydate[3],
+                         min = mydate[2],
+                         max = mydate[3]
+    )
+  })
+  # Update select column output.hru
+  observe({
+    updateSelectizeInput(session, "select_col",
+                         choices = HRUheader()[8:length(HRUheader())],
+                         selected = NULL
+  )
+  })
+  # Update select column output.hru
+  observe({
+    mydate <- input$date_range_hru
+    updateDateInput(session, "plot_date_month_hru",
+                    min = mydate[1],
+                    max = mydate[2], 
+                    value = mydate[1]
+    )
+  }) 
+  #*****************************************************************************
+    
+  
+  # --------------------------------------------------------Read output.hru data
+  outputHRU <- reactive({
+    ListOutputHRU(read.table(paste(input$TxtInOut, 
+                                "./output.hru", 
+                                sep = ""
+    ), 
+    header = FALSE,
+    sep = "",
+    skip = 9
+    )
+    )
+  })
+
+  # --------------------------------------------------------Temporal aggregation
+  outputHRUCol <- reactive({
+    selectedCol <- which(HRUheader() == input$select_col) - 1
+    outputHRUCol <-  outputHRU()[[selectedCol]]
+  })
+  
+  outputText <- reactive({
+    if (input$select_temp == 1){
+      text <- paste("You have selected to plot HRU output on  ", input$plot_date_month_hru, sep = "")
+    } else if (input$select_temp == 2){
+      text <- paste("You have selected to plot HRU output for month  ", strftime(input$plot_date_month_hru, "%m"), "/", strftime(input$plot_date_month_hru, "%Y"), sep = "")
+    } else if (input$select_temp == 3){
+      text <- paste("You have selected to plot HRU output for the year ", strftime(input$plot_date_month_hru, "%Y"), sep = "")
+    } else if (input$select_temp == 4){
+      text <- paste("You have selected to plot sum HRU output from ", input$date_range_hru[1], " to ", input$date_range_hru[2], sep = "")
+    }
+  })
+
+  output$titleHRUplot <- outputText
+
+  
+  # ------------------------------------------Get subset/ggreagate of output.hru
+  aggHRU <- reactive({
+    
+    # data column in HRU file
+    
+    subsetOutputHRU <- outputHRUCol()
+    
+    # get date of simulation period
+    evalPeriod <- seq(sim_period()[2], 
+                       sim_period()[3], 
+                       by ="days"
+    )
+    
+    # temporal aggregation daily
+    plotPeriod <- seq(input$date_range_hru[1], 
+                      input$date_range_hru[2], 
+                      by ="days"
+    )
+    
+    startIndex <- which(evalPeriod == input$date_range_hru[1] )
+    endIndex <- which(evalPeriod == input$date_range_hru[2] )
+    
+    # get subset of input data
+    subsetOutputHRU <- subsetOutputHRU[startIndex:endIndex,]
+    subsetOutputHRU <- agg(subsetOutputHRU, plotPeriod, input$select_temp)
+    
+    selectTime <- input$plot_date_month_hru
+    
+    if (input$select_temp == 1){
+      index <- which(plotPeriod == selectTime )
+      subsetOutputHRU <- subsetOutputHRU[index,]
+      
+    } else if (input$select_temp == 2){
+      
+      month <- as.numeric(strftime(selectTime, "%m"))
+      year <- as.numeric(strftime(selectTime, "%Y"))
+
+      
+      monthYear <- subsetOutputHRU[,1:2]
+      
+      for (i in 1:dim(monthYear)[1]){
+        if ((monthYear[i,1] == month) & (monthYear[i,2] == year)){
+          index <- i
+        }
+      }
+      
+      subsetOutputHRU <- subsetOutputHRU[index,-1:-2]
+      
+    } else if (input$select_temp == 3) {
+      year <- as.numeric(strftime(selectTime, "%Y"))
+      index <- which(subsetOutputHRU[,1] == year)
+      subsetOutputHRU <- subsetOutputHRU[index,-1]
+    } 
+    
+    hru <- hruRaster()
+    val <- values(hru)
+    
+    for(i in 1:length(val)){
+      val[i] <- subsetOutputHRU[val[i]]
+    }
+    
+    values(hru) <- val
+    hru <- hru
+    
+  })
+  
+  
+  # --------------------------------------------------------Temporal aggregation 
+  
+  
+  #Plot HRU map
+  output$plotHRUoutput <- renderPlot({
+    plot(aggHRU(), title("Spatial diss"))
+  })  
+  
+  
+}
---
+++ ui.R
@@ -0,0 +1,186 @@
+
+
+# Load packages
+library(shinydashboard)
+library(shinyFiles)
+
+
+# Create UI
+ui <- dashboardPage(
+  
+  # ----------------------------------------------------------------------Header
+  dashboardHeader(
+    title = "SWATShiny"
+    #titleWidth = 300
+  ),
+  
+  # ---------------------------------------------------------------------Sidebar
+  dashboardSidebar(
+    sidebarMenu(
+      menuItem("General Setting", tabName = "setting", icon = icon("fas fa-cog"))
+    ),
+    sidebarMenu(
+      menuItem("Calibration", tabName = "cali", icon = icon("align-justify"), 
+               startExpanded = TRUE, selected = TRUE,
+               menuSubItem("Manual", tabName = "manual", selected = TRUE),
+               menuSubItem("Automatic", tabName = "auto"))
+    ),
+    sidebarMenu(
+      menuItem("Visualization", tabName = "visual",icon = icon("align-justify"),
+               startExpanded = TRUE, selected = TRUE,
+               menuSubItem("watout.dat", tabName = "watout", selected = TRUE),
+               menuSubItem("output.rch", tabName = "rch", selected = TRUE),
+               menuSubItem("output.hru", tabName = "hru", selected = TRUE),
+               menuSubItem("output.sub", tabName = "outputsub"))
+    )
+  ),
+
+  # ------------------------------------------------------------------------Body
+  dashboardBody(
+    
+    # Tab item for General Setting
+    tabItems(
+      # First tab content
+      tabItem(tabName = "setting",
+              fluidRow(
+                box(
+                  textInput("Watershed", "1. Watershed directory", "./testData/Watershed"),
+                  textInput("TxtInOut", "2. TxtInOut directory", "./testData/TxtInOut"),
+                  textInput("WorkingDirectory", "3. Working directory", "./testData"),
+                ),
+                box(
+                  htmlOutput("projectInfo"),
+                )
+              ),
+              
+              tags$br(),  # Line break
+              fluidRow(
+                box(title = "HRU map", status = "success", height = "600" ,solidHeader = T,
+                    plotOutput("plotHRUmap",height = "540")),
+                
+                box(title = "Subbasin map", status = "success", height = "600" ,solidHeader = T,
+                    plotOutput("plotSubbasin",height = "540")),
+              ),
+      ),
+      
+      #*************************************************************************
+      tabItem(tabName = "hru",
+              
+              # Input data -----------------------------------------------------
+              fluidRow(             
+                column(width = 4,
+                       textInput("visualWatershed", 
+                                 "1. Watershed directory", 
+                                 "./testData/Watershed", 
+                                 width = "100%"
+                                 )
+                       ),              
+                column(width = 4,
+                       textInput("visualTxtInOut", 
+                                 "2. TxtInOut directory", 
+                                 "./testData/TxtInOut", 
+                                 width = "100%"
+                                 )
+                       ),
+                column(width = 4,
+                       selectInput("select_col", 
+                                   "3. Select data to plot", 
+                                   choices = NULL, 
+                                   width = "100%"
+                                   )
+                       )
+              ),
+              
+              fluidRow(             
+                column(width = 4,
+                       dateRangeInput("date_range_hru", 
+                                      "4. Select date range", 
+                                      width = "100%"
+                                      )
+                       ),              
+                column(width = 4,
+                       dateInput("plot_date_month_hru", 
+                                 "5. Select date/month/or year to plot", 
+                                 width = "100%"
+                                 )
+                       ),
+                column(width = 4,
+                       selectInput("select_temp", 
+                                   "6. Temporal aggregation", 
+                                   choices = list("Daily" = 1, 
+                                                  "Monthly" = 2,
+                                                  "Yearly" = 3,
+                                                  "Sum" = 4), 
+                                   selected = 1, 
+                                   width = "100%"
+                                   )
+                       )
+              ),
+            
+              
+              #textOutput("test"),
+
+              # Plot -----------------------------------------------------------              
+              tags$br(),  # Line break              
+              fluidRow(
+                box(
+                  title = textOutput('titleHRUplot'), 
+                  status = "success",
+                  height = "600" ,
+                  solidHeader = T,
+                  plotOutput("plotHRUoutput",height = "540"), 
+                  width = 12
+                  )
+                )
+              ),
+      
+      
+      #*************************************************************************
+      tabItem(tabName = "watout",
+              
+              # Input data -----------------------------------------------------
+              fluidRow(             
+                column(width = 4,
+                       fileInput("watoutFiles",
+                                 "1. Choose saveconc (e.g., watout.dat) file(s)...", 
+                                 multiple = TRUE
+                       )
+                ),
+                column(width = 4,
+                       selectInput("selectWatoutCol", 
+                                   "2. Select data to plot", 
+                                   choices = NULL, 
+                                   width = "100%"
+                       )
+                ),              
+                column(width = 4,
+                       fileInput("observedSaveconcFiles",
+                                 "3. Choose observed data file...", 
+                                 multiple = TRUE
+                       )
+                )
+              ),
+              
+              
+              fluidRow(
+                box(width = 12,
+                  sliderInput("dateRange", 
+                              "Select range to plot", 
+                              value = c(as.Date("1990-06-01","%Y-%m-%d"), 
+                                        as.Date("1993-10-01","%Y-%m-%d")), 
+                              min = as.Date("1990-06-01","%Y-%m-%d"), 
+                              max = as.Date("1993-10-01","%Y-%m-%d")
+                              )
+                  ),
+              ),
+              
+              tags$br(),  # Line break              
+              fluidRow(
+                box(title = "Simulated streamflow (m3/s)", status = "success", height = "600" ,solidHeader = T,
+                    plotOutput("plotQ",height = "540"), width = 6),
+              ),
+      )
+    )
+  ) 
+ #------------------------------------------------------------------------------ 
+)
